1dabbb34c428334ff3e115d0b93903f778b26631
TurtleFromMars <skgroup@126.com>
2013-11-03 08:57:22 +0800

    former skin.cs, still thinking about binding.
    problem: OptionPanel needs an initialized Skin when binding, otherwise,
    implementation of the interface INotifyPropertyChanged could be
    essential.

--- TXTReader/Data/Skin.cs
+++ TXTReader/Data/Skin.cs
@@ -1,28 +1,40 @@
-<<<<<<< HEAD
-﻿using System;
 using System.Collections.Generic;
-=======
-﻿using System.Collections.Generic;
->>>>>>> origin/master
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using System;
+using System.Xml;
 
-namespace TXTReader.Data {
+namespace TXTReader.Data
+{
 
-<<<<<<< HEAD
-    enum BackGroundType { SolidColor, Image }
-    enum TextEffect { None, Shadow, Stroke }//无，阴影，描边
-
-    class Skin {
-=======
     public enum BackGroundType { SolidColor, Image }
     public enum EffectType { None, Shadow, Stroke }//无，阴影，描边
 
-    public class Skin {
->>>>>>> origin/master
+    public class Skin
+    {
+
+        //(NOT SOLVED)
+        //the problem of binding in optionpanel.
+        public Skin()
+        {
+            //thought about SkinParser but obviously it doesn't work here.
+
+            //LineSpacing = 4;
+            //ParaSpacing = 8;
+            //Font = new Typeface("宋体");
+            //FontSize = 12;
+            //Foreground = Brushes.Yellow;
+            //BackColor = Colors.DarkBlue;
+            //BackGroundType = BackGroundType.SolidColor;
+            //Padding = new Thickness(16);
+            //EffetSize = 1;
+            //Effect = Brushes.Black;
+            //EffectType = EffectType.Shadow;
+        }
+
         private BackGroundType backGroundType;
         private Brush background = null;
 
@@ -30,52 +42,44 @@ namespace TXTReader.Data {
 
         public Brush Foreground { get; set; }//FontColor
         public Typeface Font { get; set; }
-<<<<<<< HEAD
-        public bool IsBold { get; set; }
-        public bool IsItalic { get; set; }
-        public bool IsUnderLine { get; set; }
-        public TextEffect TextEffect { get; set; }//字体特效
-        public Color EffetColor { get; set; }//特效所用的颜色
-
-        public int LineSpacing { get; set; }//行间距
-        public int paraspacing { get; set; }//段间距
-=======
         public double FontSize { get; set; }
         public EffectType EffectType { get; set; }//字体特效
         public Brush Effect { get; set; }//特效所用的颜色
+
+        ///////////////////////////////////////////////////////////////////
+        //recommended to fix.
         public double EffetSize { get; set; }//特效的大小
+        //spelling mistake...?
+        //Though I turned out to use the name "EffetSize" in my implementation of OptionPanel.
+        ///////////////////////////////////////////////////////////////////
 
         public double LineSpacing { get; set; }//行间距
         public double ParaSpacing { get; set; }//段间距
->>>>>>> origin/master
 
         public Color BackColor { get; set; }//背景颜色
         public ImageSource BackImage { get; set; }//背景图片
-        public BackGroundType BackGroundType {
+        public BackGroundType BackGroundType
+        {
             get { return backGroundType; }
-            set {
-                if (backGroundType != value) {
-                    backGroundType = value; 
+            set
+            {
+                if (backGroundType != value)
+                {
+                    backGroundType = value;
                     background = null;
                 }
             }
         }
-        public Brush Background {
-            get {
-<<<<<<< HEAD
-                if (background==null) return background;
+        public Brush Background
+        {
+            get
+            {
+                if (background != null) return background;
                 switch (BackGroundType)
                 {
-                case BackGroundType.SolidColor:return new SolidColorBrush(BackColor);
-                case BackGroundType.Image: return new ImageBrush(BackImage);
-=======
-                if (background!=null) return background;
-                switch (BackGroundType)
-                {
-                case BackGroundType.SolidColor:return background = new SolidColorBrush(BackColor);
-                case BackGroundType.Image: return background = new ImageBrush(BackImage);
->>>>>>> origin/master
-                default: return null;
+                    case BackGroundType.SolidColor: return background = new SolidColorBrush(BackColor);
+                    case BackGroundType.Image: return background = new ImageBrush(BackImage);
+                    default: return null;
                 }
             }
         }
