506d10abb1013cbc922109570f201ddde762c518
TurtleFromMars <skgroup@126.com>
2013-11-03 09:18:22 +0800

    Added StandardColorPicker. and reference to Microsoft.Expression.Effects 4.5.0.0

---
+++ TXTReader/Widget/StandardColorPicker.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace TXTReader.Widget
+{
+    /// <summary>
+    /// StandardColorPicker.xaml 的交互逻辑
+    /// A UserControl shown as a customized button 
+    /// which can call a System.Windows.Forms.ColorDialog .
+    /// A WPF-wrap of the WinForm's ColorDialog.
+    /// Property:
+    ///     [DP] (System.Windows.Media.Color) SelectedColor
+    /// Event:
+    ///     SelectedColorChanged
+    ///     
+    /// </summary>
+    public partial class StandardColorPicker : UserControl
+    {
+        //used to save custom colors in the corresponding ColorDialog.
+        private int[] customColorsFromDialog;
+
+        public StandardColorPicker()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty SelectedColorProperty =
+            DependencyProperty.Register("SelectedColor", typeof(Color), typeof(StandardColorPicker),
+                new PropertyMetadata(new PropertyChangedCallback(SelectedColorPropertyChangedCallback)));
+
+        public Color SelectedColor
+        {
+            get
+            {
+                return (Color)GetValue(SelectedColorProperty);
+            }
+
+            set
+            {
+                SetValue(SelectedColorProperty, value);
+            }
+        }
+
+        private static void SelectedColorPropertyChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs arg)
+        {
+            if (sender != null && sender is StandardColorPicker)
+            {
+                StandardColorPicker picker = sender as StandardColorPicker;
+                picker.OnSelectedColorChanged((Color)arg.OldValue, (Color)arg.NewValue);
+
+            }
+        }
+
+        public static readonly RoutedEvent SelectedColorChangedEvent =
+            EventManager.RegisterRoutedEvent("SelectedColorChanged",
+             RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<Color>), typeof(StandardColorPicker));
+
+        public event RoutedPropertyChangedEventHandler<Color> SelectedColorChanged
+        {
+            add
+            {
+                this.AddHandler(SelectedColorChangedEvent, value);
+            }
+            remove
+            {
+                this.RemoveHandler(SelectedColorChangedEvent, value);
+            }
+        }
+
+        protected virtual void OnSelectedColorChanged(Color oldValue, Color newValue)
+        {
+            //update displayed color if necessary
+
+            RoutedPropertyChangedEventArgs<Color> arg =
+                new RoutedPropertyChangedEventArgs<Color>(oldValue, newValue, SelectedColorChangedEvent);
+            this.RaiseEvent(arg);
+
+        }
+
+        private void btnChooseColor_Click(object sender, RoutedEventArgs e)
+        {
+            //set and show a ColorDialog.
+            System.Windows.Forms.ColorDialog dlg = new System.Windows.Forms.ColorDialog();
+            dlg.Color = WpfColorToWinformColor(SelectedColor);
+            dlg.FullOpen = true;
+            if (customColorsFromDialog != null)
+            {
+                dlg.CustomColors = customColorsFromDialog;
+            }
+            //change SelectedColor according to the result of the dialog.
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                SelectedColor = WinformColorToWpfColor(dlg.Color);
+            }
+            //save custom colors.
+            customColorsFromDialog = dlg.CustomColors;
+        }
+
+        //a method used to get a new WinForm Color from a WPF Color.
+        private System.Drawing.Color WpfColorToWinformColor(System.Windows.Media.Color wpfColor)
+        {
+            return System.Drawing.ColorTranslator.FromHtml(wpfColor.ToString());
+        }
+
+        //a method used to get a new WPF Color from a WinForm Color.
+        private System.Windows.Media.Color WinformColorToWpfColor(System.Drawing.Color winformColor)
+        {
+            System.Windows.Media.Color wpfColor = new System.Windows.Media.Color();
+            wpfColor.A = winformColor.A;
+            wpfColor.R = winformColor.R;
+            wpfColor.G = winformColor.G;
+            wpfColor.B = winformColor.B;
+            return wpfColor;
+        }
+    }
+}
