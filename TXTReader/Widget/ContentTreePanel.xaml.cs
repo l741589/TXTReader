6bacb0d230f04187e4438863044e3433630d74c3
TurtleFromMars <skgroup@126.com>
2013-12-08 03:38:06 +0800

    [BETA]Added ContentTreePanel
    [BETA] Able to display after calling UpdateContentUI method. UI update
    problem to be solved. DataTemplate layout needs to be improved.

---
+++ TXTReader/Widget/ContentTreePanel.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TXTReader.Widget
+{
+    /// <summary>
+    /// ContentTreePanel.xaml 的交互逻辑
+    /// </summary>
+    public partial class ContentTreePanel : UserControl
+    {
+        public ContentTreePanel()
+        {
+            InitializeComponent();
+        }
+
+        private void trvContent_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            Data.ContentAdapter selectedContent = sender as Data.ContentAdapter;
+            if (selectedContent != null)
+            {
+
+            }
+        }
+
+        //There are some problems of setting binding to a static class in XAML.
+        //(mainly because a binding breaks() when the binding source is set to null.)
+        //So, [CALL THIS METHOD] when Utility.G.Book is changed.
+        public void UpdateContentUI()
+        {
+            txbTitle.SetBinding(TextBlock.TextProperty, new Binding("Title") { Source = Utility.G.Book });
+            txbLength.SetBinding(TextBlock.TextProperty, new Binding("Length") { Source = Utility.G.Book });
+            trvContent.SetBinding(TreeView.ItemsSourceProperty, new Binding("Children") { Source = Utility.G.Book });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateContentUI();
+        }
+    }
+}
